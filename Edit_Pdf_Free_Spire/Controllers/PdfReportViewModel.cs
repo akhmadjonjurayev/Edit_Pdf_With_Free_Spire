7d12b120bd807e07b91f29ed55ec2a12da450860
Akhmadjon <formyproject1999@gmail.com>
2021-07-07 11:52:53 +0500

    first commit

---
+++ Edit_Pdf_Free_Spire/Controllers/PdfReportViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edit_Pdf_Free_Spire.Controllers
+{
+    public class PdfReportViewModel
+    {
+        public bool IsFirstPage { get; set; }
+        public Location Image_Location { get; set; }
+        public int Image_Weigth { get; set; }
+        public int Image_Heigth { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public string PdfName { get; set; }
+    }
+    public class Location
+    {
+        public int Position_X { get; set; }
+        public int Position_Y { get; set; }
+    }
+}
---
+++ Edit_Pdf_Free_Spire/Controllers/WeatherForecastController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edit_Pdf_Free_Spire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly IWebHostEnvironment _web;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IWebHostEnvironment web)
+        {
+            _web = web;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpPost("editpdf")]
+        public IActionResult EditPDf([FromBody] PdfReportViewModel pdfReport)
+        {
+            PdfDocument document = new PdfDocument();
+            var path = string.Concat(_web.WebRootPath, @"\resourse\report.pdf");
+            document.LoadFromFile(path);
+            var img = PdfImage.FromStream(DrawImage(pdfReport.Year,pdfReport.Month));
+            if (pdfReport.IsFirstPage)
+            {
+                document.Pages[0].Canvas.DrawImage(img, pdfReport.Image_Location.Position_X, pdfReport.Image_Location.Position_Y);
+            }
+            else
+            {
+                int count = document.Pages.Count;
+                document.Pages[count - 1].Canvas.DrawImage(img, pdfReport.Image_Location.Position_X, pdfReport.Image_Location.Position_Y);
+            }
+            var pdf = new MemoryStream();
+            document.SaveToStream(pdf, FileFormat.PDF);
+            return File(pdf.ToArray(), "application/pdf", "hisobot.pdf");
+        }
+        private MemoryStream DrawImage(int Year,int Month)
+        {
+            var path = string.Concat(_web.WebRootPath, @"\resourse\pechat_kirish.png");
+            Image image = System.Drawing.Image.FromFile(path);
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                graphics.DrawString(Year.ToString(), new Font("Tahoma", 8), Brushes.Blue, 125f, 108f);
+                graphics.DrawString(Month.ToString(), new Font("Tahoma", 8), Brushes.Blue, 250f, 108f);
+                var ms = new MemoryStream();
+                image.Save(ms, ImageFormat.Png);
+                return ms;
+            }
+        }
+    }
+}
