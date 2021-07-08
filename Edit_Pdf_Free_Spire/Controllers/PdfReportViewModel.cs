23728d5745ccf989c39fdd5482a6a191b29a8995
Akhmadjon <formyproject1999@gmail.com>
2021-07-08 10:01:07 +0500

    Refactoring

--- Edit_Pdf_Free_Spire/Controllers/PdfReportViewModel.cs
+++ Edit_Pdf_Free_Spire/Controllers/PdfReportViewModel.cs
@@ -9,16 +9,20 @@ namespace Edit_Pdf_Free_Spire.Controllers
     {
         public bool IsFirstPage { get; set; }
         public Location Image_Location { get; set; }
-        public int Image_Weigth { get; set; }
-        public int Image_Heigth { get; set; }
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public int Day { get; set; }
+        public IEnumerable<Location_Variable> location_Variables { get; set; }
         public string PdfName { get; set; }
     }
     public class Location
     {
         public int Position_X { get; set; }
         public int Position_Y { get; set; }
+    }
+    public class Location_Variable
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public Location Location { get; set; }
+        public string Color { get; set; } = "Blue";
+        public int Size { get; set; }
     }
 }
--- Edit_Pdf_Free_Spire/Controllers/WeatherForecastController.cs
+++ Edit_Pdf_Free_Spire/Controllers/WeatherForecastController.cs
@@ -9,7 +9,6 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Edit_Pdf_Free_Spire.Controllers
 {
@@ -46,9 +45,9 @@ namespace Edit_Pdf_Free_Spire.Controllers
         public IActionResult EditPDf([FromBody] PdfReportViewModel pdfReport)
         {
             PdfDocument document = new PdfDocument();
-            var path = string.Concat(_web.WebRootPath, @"\resourse\report.pdf");
+            string path = string.Concat(_web.WebRootPath, @"\resourse\report.pdf");
             document.LoadFromFile(path);
-            var img = PdfImage.FromStream(DrawImage(pdfReport.Year,pdfReport.Month));
+            var img = PdfImage.FromStream(DrawImage(pdfReport.location_Variables));
             if (pdfReport.IsFirstPage)
             {
                 document.Pages[0].Canvas.DrawImage(img, pdfReport.Image_Location.Position_X, pdfReport.Image_Location.Position_Y);
@@ -60,20 +59,35 @@ namespace Edit_Pdf_Free_Spire.Controllers
             }
             var pdf = new MemoryStream();
             document.SaveToStream(pdf, FileFormat.PDF);
-            return File(pdf.ToArray(), "application/pdf", "hisobot.pdf");
+            return File(pdf.ToArray(), "application/pdf", pdfReport.PdfName);
         }
-        private MemoryStream DrawImage(int Year,int Month)
+        private MemoryStream DrawImage(IEnumerable<Location_Variable> location_Variables)
         {
-            var path = string.Concat(_web.WebRootPath, @"\resourse\pechat_kirish.png");
-            Image image = System.Drawing.Image.FromFile(path);
+            string path = string.Concat(_web.WebRootPath, @"\resourse\pechat_kirish.png");
+            Image image = Image.FromFile(path);
             using (Graphics graphics = Graphics.FromImage(image))
             {
-                graphics.DrawString(Year.ToString(), new Font("Tahoma", 8), Brushes.Blue, 125f, 108f);
-                graphics.DrawString(Month.ToString(), new Font("Tahoma", 8), Brushes.Blue, 250f, 108f);
+                foreach(var item in location_Variables)
+                {
+                    graphics.DrawString(item.Value.ToString(), new Font("Tahoma", item.Size), GetBrushes(item.Color), item.Location.Position_X, item.Location.Position_Y);
+                }
                 var ms = new MemoryStream();
                 image.Save(ms, ImageFormat.Png);
                 return ms;
             }
+        }
+        private Brush GetBrushes(string color)
+        {
+            Brush brush;
+            switch(color.ToLower())
+            {
+                case "red":brush = Brushes.Red;break;
+                case "black":brush = Brushes.Black;break;
+                case "pink":brush = Brushes.Pink;break;
+                case "yellow":brush = Brushes.Yellow;break;
+                default:brush = Brushes.Blue;break;
+            }
+            return brush;
         }
     }
 }
